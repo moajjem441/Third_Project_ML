da053402431aa2ebddcfc0f10d333ca74b31d79f
Moajjem Hossain <moajjem441@gmail.com>
2026-04-17 19:47:03 +0600

    some part changes

--- Second-try.py
+++ Second-try.py
@@ -56,6 +56,7 @@ while cap.isOpened():
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, label, (x1, y1 - 10), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                        
 
     cv2.imshow("Third Eye", frame)
 
